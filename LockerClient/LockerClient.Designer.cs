11d7c3985d5c794470228227deb3086f2dcf1b62
HarrisQs <harris32916@gmail.com>
2016-05-27 06:51:02 +0800

    the frame of client is OK.

---
+++ LockerClient/LockerClient.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LockerClient
+{
+    partial class LockerClient
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextBoxConsole = new System.Windows.Forms.RichTextBox();
+            this.TextBoxMessage = new System.Windows.Forms.TextBox();
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RichTextBoxConsole
+            // 
+            this.RichTextBoxConsole.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.RichTextBoxConsole.Location = new System.Drawing.Point(10, 37);
+            this.RichTextBoxConsole.Name = "RichTextBoxConsole";
+            this.RichTextBoxConsole.ReadOnly = true;
+            this.RichTextBoxConsole.Size = new System.Drawing.Size(481, 426);
+            this.RichTextBoxConsole.TabIndex = 6;
+            this.RichTextBoxConsole.Text = "";
+            // 
+            // TextBoxMessage
+            // 
+            this.TextBoxMessage.Location = new System.Drawing.Point(10, 13);
+            this.TextBoxMessage.Name = "TextBoxMessage";
+            this.TextBoxMessage.Size = new System.Drawing.Size(400, 22);
+            this.TextBoxMessage.TabIndex = 5;
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.Enabled = false;
+            this.ButtonSend.Location = new System.Drawing.Point(416, 11);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(75, 21);
+            this.ButtonSend.TabIndex = 4;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
+            // 
+            // LockerClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 474);
+            this.Controls.Add(this.RichTextBoxConsole);
+            this.Controls.Add(this.TextBoxMessage);
+            this.Controls.Add(this.ButtonSend);
+            this.Name = "LockerClient";
+            this.Text = "Locker Clinet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextBoxConsole;
+        private System.Windows.Forms.TextBox TextBoxMessage;
+        private System.Windows.Forms.Button ButtonSend;
+
+    }
+}
+
