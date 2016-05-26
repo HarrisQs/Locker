165329b0ff111bdb08a8dc13ef4cb44ce16506fa
HarrisQs <harris32916@gmail.com>
2016-05-26 22:59:43 +0800

    Server is OK.

---
+++ Locker/LockerServer.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Locker
+{
+    partial class LockerServer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextBoxConsole = new System.Windows.Forms.RichTextBox();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RichTextBoxConsole
+            // 
+            this.RichTextBoxConsole.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.RichTextBoxConsole.Location = new System.Drawing.Point(13, 39);
+            this.RichTextBoxConsole.Name = "RichTextBoxConsole";
+            this.RichTextBoxConsole.ReadOnly = true;
+            this.RichTextBoxConsole.Size = new System.Drawing.Size(302, 209);
+            this.RichTextBoxConsole.TabIndex = 4;
+            this.RichTextBoxConsole.Text = "";
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Enabled = false;
+            this.ButtonStop.Location = new System.Drawing.Point(205, 12);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(110, 21);
+            this.ButtonStop.TabIndex = 2;
+            this.ButtonStop.Text = "Stop";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(13, 12);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(110, 21);
+            this.ButtonStart.TabIndex = 3;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // LockerServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 258);
+            this.Controls.Add(this.RichTextBoxConsole);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ButtonStart);
+            this.Name = "LockerServer";
+            this.Text = "LockerServer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LockerServer_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextBoxConsole;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Locker
 簡易的電腦使用者管理系統
+
+# Project Name
+## Synopsis
+
+At the top of the file there should be a short introduction and/ or overview that explains **what** the project is. This description should match descriptions added for package managers (Gemspec, package.json, etc.)
+
+## Code Need
+
+Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+
+## Motivation
+
+A short description of the motivation behind the creation and maintenance of the project. This should explain **why** the project exists.
+
+## Installation
+
+Provide code examples and explanations of how to get the project.
+
+## API Reference
+
+* [Using SignalR in WinForms and WPF](https://code.msdn.microsoft.com/windowsdesktop/Using-SignalR-in-WinForms-f1ec847b)
+* [Microsoft ASP.NET SignalR 2.2.0](https://www.nuget.org/packages/Microsoft.AspNet.SignalR/2.2.0)
+* [Microsoft.Owin.Cors 3.0.1](https://www.nuget.org/packages/Microsoft.Owin.Cors/)
+* [Microsoft.Owin.Hosting 3.0.1](https://www.nuget.org/packages/Microsoft.Owin.Hosting/)
+* [Microsoft.Owin.Host.HttpListener 3.0.1](https://www.nuget.org/packages/Microsoft.Owin.Host.HttpListener/)
+* [啟動 SignalR 發生 System.Reflection.TargetInvocationException 例外](https://dotblogs.com.tw/joysdw12/archive/2013/11/26/aspnet-signalr-system-reflection-targetinvocationexception.aspx)
+* [OWIN](http://blog.darkthread.net/post-2013-12-01-about-owin.aspx)
+* [SignalR causing major issues on upgrade](http://stackoverflow.com/questions/19814213/signalr-causing-major-issues-on-upgrade)
+
+## Tests
+
+Describe and show how to run the tests with code examples.
+
+## History
+
+TODO: Write history
+
+##FAQ
+
+## Contributors
+
+Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
+
+## License
+
+Available under [the MIT license](https://mths.be/mit).
