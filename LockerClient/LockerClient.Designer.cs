0339f4f6d8144fba28996046f96c51a1ef42c411
HarrisQs <harris32916@gmail.com>
2016-05-27 16:57:35 +0800

    Dynamically move frame.

--- LockerClient/LockerClient.Designer.cs
+++ LockerClient/LockerClient.Designer.cs
@@ -35,8 +35,11 @@
             this.Account_label = new System.Windows.Forms.Label();
             this.Account_textBox = new System.Windows.Forms.TextBox();
             this.Password_textBox = new System.Windows.Forms.TextBox();
-            this.Enter_button = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.EnterTempPassword_button = new System.Windows.Forms.Button();
+            this.Term_Checkbox = new System.Windows.Forms.CheckBox();
+            this.TemporaryPassword_textBox = new System.Windows.Forms.TextBox();
+            this.LoginFailed_label = new System.Windows.Forms.Label();
+            this.Login_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Login_pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ConnectFailed_pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Loading_pictureBox)).BeginInit();
@@ -115,43 +118,78 @@
             this.Password_textBox.Size = new System.Drawing.Size(100, 33);
             this.Password_textBox.TabIndex = 15;
             // 
-            // Enter_button
+            // EnterTempPassword_button
             // 
-            this.Enter_button.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Enter_button.ForeColor = System.Drawing.Color.Black;
-            this.Enter_button.Location = new System.Drawing.Point(52, 328);
-            this.Enter_button.Name = "Enter_button";
-            this.Enter_button.Size = new System.Drawing.Size(172, 57);
-            this.Enter_button.TabIndex = 16;
-            this.Enter_button.Text = "Enter";
-            this.Enter_button.UseVisualStyleBackColor = true;
-            this.Enter_button.Visible = false;
+            this.EnterTempPassword_button.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.EnterTempPassword_button.ForeColor = System.Drawing.Color.Black;
+            this.EnterTempPassword_button.Location = new System.Drawing.Point(52, 328);
+            this.EnterTempPassword_button.Name = "EnterTempPassword_button";
+            this.EnterTempPassword_button.Size = new System.Drawing.Size(172, 57);
+            this.EnterTempPassword_button.TabIndex = 16;
+            this.EnterTempPassword_button.Text = "Enter";
+            this.EnterTempPassword_button.UseVisualStyleBackColor = true;
+            this.EnterTempPassword_button.Visible = false;
             // 
-            // checkBox1
+            // Term_Checkbox
             // 
-            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.Term_Checkbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.checkBox1.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.checkBox1.Location = new System.Drawing.Point(12, 422);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(353, 20);
-            this.checkBox1.TabIndex = 17;
-            this.checkBox1.Text = "Yes,I have read this Agreement and agree to the terms.";
-            this.checkBox1.UseVisualStyleBackColor = false;
+            this.Term_Checkbox.AutoSize = true;
+            this.Term_Checkbox.BackColor = System.Drawing.SystemColors.Window;
+            this.Term_Checkbox.Checked = true;
+            this.Term_Checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Term_Checkbox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Term_Checkbox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Term_Checkbox.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Term_Checkbox.Location = new System.Drawing.Point(12, 422);
+            this.Term_Checkbox.Name = "Term_Checkbox";
+            this.Term_Checkbox.Size = new System.Drawing.Size(353, 20);
+            this.Term_Checkbox.TabIndex = 17;
+            this.Term_Checkbox.Text = "Yes,I have read this Agreement and agree to the terms.";
+            this.Term_Checkbox.UseVisualStyleBackColor = false;
+            // 
+            // TemporaryPassword_textBox
+            // 
+            this.TemporaryPassword_textBox.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TemporaryPassword_textBox.Location = new System.Drawing.Point(254, 250);
+            this.TemporaryPassword_textBox.Name = "TemporaryPassword_textBox";
+            this.TemporaryPassword_textBox.Size = new System.Drawing.Size(100, 33);
+            this.TemporaryPassword_textBox.TabIndex = 18;
+            this.TemporaryPassword_textBox.Visible = false;
+            // 
+            // LoginFailed_label
+            // 
+            this.LoginFailed_label.AutoSize = true;
+            this.LoginFailed_label.BackColor = System.Drawing.Color.White;
+            this.LoginFailed_label.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.LoginFailed_label.ForeColor = System.Drawing.Color.DarkRed;
+            this.LoginFailed_label.Location = new System.Drawing.Point(250, 225);
+            this.LoginFailed_label.Name = "LoginFailed_label";
+            this.LoginFailed_label.Size = new System.Drawing.Size(86, 24);
+            this.LoginFailed_label.TabIndex = 19;
+            this.LoginFailed_label.Text = "登入失敗";
+            // 
+            // Login_button
+            // 
+            this.Login_button.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Login_button.ForeColor = System.Drawing.Color.Black;
+            this.Login_button.Location = new System.Drawing.Point(274, 328);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(172, 57);
+            this.Login_button.TabIndex = 20;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = true;
             // 
             // LockerClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 474);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.Enter_button);
+            this.Controls.Add(this.Login_button);
+            this.Controls.Add(this.LoginFailed_label);
+            this.Controls.Add(this.TemporaryPassword_textBox);
+            this.Controls.Add(this.Term_Checkbox);
+            this.Controls.Add(this.EnterTempPassword_button);
             this.Controls.Add(this.Password_textBox);
             this.Controls.Add(this.Account_textBox);
             this.Controls.Add(this.Account_label);
@@ -161,7 +199,7 @@
             this.Controls.Add(this.Login_pictureBox);
             this.Name = "LockerClient";
             this.Text = "Locker Clinet";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WinFormsClient_FormClosing);
+            this.Load += new System.EventHandler(this.LockerClient_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Login_pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ConnectFailed_pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Loading_pictureBox)).EndInit();
@@ -179,8 +217,11 @@
         private System.Windows.Forms.Label Account_label;
         private System.Windows.Forms.TextBox Account_textBox;
         private System.Windows.Forms.TextBox Password_textBox;
-        private System.Windows.Forms.Button Enter_button;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button EnterTempPassword_button;
+        private System.Windows.Forms.CheckBox Term_Checkbox;
+        private System.Windows.Forms.TextBox TemporaryPassword_textBox;
+        private System.Windows.Forms.Label LoginFailed_label;
+        private System.Windows.Forms.Button Login_button;
 
 
 
--- LockerClient/LockerClient.cs
+++ LockerClient/LockerClient.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/**
+ * 撰寫時間:2015/02/07
+ * 作者:張弘瑜
+ * 目標:做一個SignalR的登入客戶端
+ **/ 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,24 +19,62 @@ namespace LockerClient
 {
     public partial class LockerClient : Form
     {
-
-        private String UserName { get; set; }
+        private const string ServerURI = "http://localhost:8080/signalr";
         private IHubProxy HubProxy { get; set; }
-        const string ServerURI = "http://localhost:8080/signalr";
         private HubConnection Connection { get; set; }
+
+        //記錄Login畫面和螢幕的中心位置
+        private int Login_X = 0;
+        private int Login_Y = 0;
+        private int ScreenCenter_X = 0;
+        private int ScreenCenter_Y = 0;
 
         internal LockerClient()
         {
             InitializeComponent();
+            //UI設定
+            //this.FormBorderStyle = FormBorderStyle.None;//把上放的列關掉
+            this.Location = new Point(0, 0);//Form 出現在左上角
+            this.BackColor = System.Drawing.Color.Black;//背景黑色
+            this.Height = Screen.PrimaryScreen.Bounds.Height;//Form 的高
+            this.Width = Screen.PrimaryScreen.Bounds.Width;//Form 的寬
+            Login_pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;//Login AutoSize
+            ConnectFailed_pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;//Success AutoSize
+            ConnectFailed_pictureBox.Visible = false;
+            Loading_pictureBox.Visible = false;
+            Loading_pictureBox.BackColor = System.Drawing.Color.White;
+            Account_textBox.Height = 33;
+            Account_textBox.Width = 250;
+            Password_textBox.Height = 33;
+            Password_textBox.Width = 250;
+            TemporaryPassword_textBox.Height = 33;
+            TemporaryPassword_textBox.Width = 150;
+            LoginFailed_label.Visible = false;
+            Login_button.ForeColor = System.Drawing.Color.Silver;
+
         }
 
-        private void WinFormsClient_FormClosing(object sender, FormClosingEventArgs e)
+        private void LockerClient_Load(object sender, EventArgs e)
         {
-            if (Connection != null)
-            {
-                Connection.Stop();
-                Connection.Dispose();
-            }
+            //LOGIN畫面 的位置
+            ScreenCenter_X = Screen.PrimaryScreen.Bounds.Width / 2;
+            ScreenCenter_Y = Screen.PrimaryScreen.Bounds.Height / 2;
+            Login_X = ScreenCenter_X - Login_pictureBox.Size.Width / 2;
+            Login_Y = ScreenCenter_Y - Login_pictureBox.Size.Height / 2;
+            Login_pictureBox.Location = new Point(Login_X, Login_Y);
+            //帳號密碼登入的位置
+            Account_label.Location = new Point(ScreenCenter_X - 211, ScreenCenter_Y - 42);
+            Password_label.Location = new Point(ScreenCenter_X - 211, ScreenCenter_Y + 38);
+            Account_textBox.Location = new Point(ScreenCenter_X - 97, ScreenCenter_Y - 24);
+            Password_textBox.Location = new Point(ScreenCenter_X - 97, ScreenCenter_Y + 44);
+            Login_button.Location = new Point(ScreenCenter_X - 78, ScreenCenter_Y + 130);
+            //載入畫面的位置
+            Loading_pictureBox.Location = new Point(ScreenCenter_X - 65, ScreenCenter_Y + 95);
+            Loading_pictureBox.Visible = true;
+            //登入失敗訊息的位置 
+            LoginFailed_label.Location = new Point(ScreenCenter_X - 90, ScreenCenter_Y + 95);
+            // show services term
+            Term_Checkbox.Location = new Point(ScreenCenter_X - 150, ScreenCenter_Y + 200);
         }
 
     }
