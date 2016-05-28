9dfaf1679932455ba6f6a0ff80687cb9a6bd05f1
HarrisQs <harris32916@gmail.com>
2016-05-28 09:45:43 +0800

    Basic Temporary Password Form

---
+++ TemporaryPassword/LockerTemporaryPassword.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemporaryPassword
+{
+    public partial class TemporaryPasswordForm : Form
+    {
+        string source = Convert.ToString(DateTime.Today);
+        public TemporaryPasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(TemporaryPassword() + "\n");
+        }
+        private string TemporaryPassword()
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string hash = GetMd5Hash(md5Hash, source);
+                return hash;
+            }
+        }
+
+        private static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 10; i < 14; i += 2)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            source = Convert.ToString(dateTimePicker1.Value.Date);
+        }
+    }
+}
